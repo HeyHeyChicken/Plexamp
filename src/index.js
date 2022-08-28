9c2a3b94c64d89818f173dcaa6ac97eda076417d
Antoine Duval <antoine@cuffel.fr>
2022-08-28 18:39:04 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ class Facebook extends LIBRARIES.Skill {
 
     console.log(_settings);
 
+    if(_settings.token != null && _settings.token != ""){
+      axios.post(_settings.serverIP + ":" + _settings.serverPort + "/library/sections/?X-Plex-Token=" + _settings.token)
+        .then((res) => {
+          console.log(`Status: ${res.status}`);
+          console.log('Body: ', res.data);
+        }).catch((err) => {
+          console.error(err);
+        }
+      );
+    }
+
     this.Main.Manager.addAction("Facebook.post", function(_intent, _socket){
       // Here you will add an action when the user want to post anything on Facebook.
       // You can access utterances variables like this : _intent.Variables.text
