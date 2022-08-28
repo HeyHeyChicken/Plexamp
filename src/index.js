9f7e249c096a9bc624b703f5af68fbfe4a737d00
Antoine Duval <antoine@cuffel.fr>
2022-08-28 18:41:17 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Facebook extends LIBRARIES.Skill {
     console.log(_settings);
 
     if(_settings.token != null && _settings.token != ""){
-      axios.post(_settings.serverIP + ":" + _settings.serverPort + "/library/sections/?X-Plex-Token=" + _settings.token)
+      LIBRARIES.Axios.get(_settings.serverIP + ":" + _settings.serverPort + "/library/sections/?X-Plex-Token=" + _settings.token)
         .then((res) => {
           console.log(`Status: ${res.status}`);
           console.log('Body: ', res.data);
