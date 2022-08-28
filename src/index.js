1cf1be56b71a324457e6ff35a21667ca4c8f1944
Antoine Duval <antoine@cuffel.fr>
2022-08-28 18:36:30 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const LIBRARIES = {
-  Skill: require("../../../Libraries/Skill")
+  Skill: require("../../../Libraries/Skill"),
+  Axios: require("axios")
 };
 
 class Facebook extends LIBRARIES.Skill {
   constructor(_main, _settings) {
     super(_main, _settings);
     const SELF = this;
+
+    console.log(_settings);
 
     this.Main.Manager.addAction("Facebook.post", function(_intent, _socket){
       // Here you will add an action when the user want to post anything on Facebook.
